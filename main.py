d3a8a261d24dd3d6de2d0ef8c6da39e27f4f8e7f
David Schulte <ds62myry@studserv.uni-leipzig.de>
2022-06-10 22:43:33 +0200

    Working socket

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import tornado.template
+
 
 class GameSocketHandler(tornado.websocket.WebSocketHandler):
 
@@ -9,20 +11,28 @@ class GameSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         print(message)
+        self.write_message(message)
 
     def on_close(self):
         print("Socket closed")
 
+    def sendUpdateData(self, data):
+        self.write_message(data)
+        
+
 class MainRequesHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write("Hello World")
+        loader = tornado.template.Loader("./templates/")
+        self.write(loader.load('index.html').generate())
 
 
 if __name__ == '__main__':
     application = tornado.web.Application([
         (r'/', MainRequesHandler),
-    ])
+        (r'/ws', GameSocketHandler),
+        (r'/assets/(.)?', tornado.web.StaticFileHandler, {"path": "./assets/"}),
+    ], debug=True)
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
     
